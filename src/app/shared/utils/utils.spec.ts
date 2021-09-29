56709d8c07bdc4a586e34c0b0a807d20edac17e7
Maria Alejandra Bohorquez Martheyn - Ceiba Software <maria.bohorquez@ceiba.com.co>
2021-09-29 18:40:32 -0500

    se empeizan test con cypress

--- src/app/shared/utils/utils.spec.ts
+++
@@ -1,9 +0,0 @@
-import {currencyFormat} from './utils';
-
-it('se ejecuta currencyFormat correctamente', () => {
-   const currency = 500;
-   const spec = '$500.00';
-   expect(currencyFormat(currency)).toBe(spec);
-  });
-
-  
