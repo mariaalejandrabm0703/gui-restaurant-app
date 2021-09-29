a1f44f3707e94a42373ee30db724813876999a89
Maria Alejandra Bohorquez Martheyn - Ceiba Software <maria.bohorquez@ceiba.com.co>
2021-09-28 22:15:18 -0500

    se adiciona unit test de productsReducer.

---
+++ src/app/core/redux/reductores/products/productsReducer.spec.ts
@@ -0,0 +1,30 @@
+import {defaultState, setProducts} from '../../actions/products/ActionsProducts';
+import { IProduct } from '../../../../feature/Home/models/Home';
+import {IStateProducts} from '../../modelo/IStateProducts';
+import { productInfo } from '../../../../shared/utils/data';
+import productsReducer from './productsReducer';
+
+describe('Reductor de productos', () => {
+    const prods: Array<IProduct> = [productInfo];
+
+    it('debería retornar el estado por defecto', () => {
+        const initialState: IStateProducts = {
+            listProducts: Array<IProduct>(),
+        };
+    
+        const newState = productsReducer(initialState, defaultState(prods));
+        expect(newState).toStrictEqual(initialState);
+      });
+      it('debería retornar establecer todos los productos', () => {
+        const initialState: IStateProducts = {
+            listProducts: Array<IProduct>(),
+        };
+    
+        const expectedState: IStateProducts = {
+            listProducts: prods
+          };
+    
+        const newState = productsReducer(initialState, setProducts(prods));
+        expect(newState).toStrictEqual(expectedState);
+      });
+});
